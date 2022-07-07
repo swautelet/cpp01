40c19b6bc7a7824292b6227b8f57c4f7a28747ef
swautelet <swautele@student.s19.be>
2022-07-07 18:09:10 +0200

    save

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+	std::string brain = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &brain;
+	std::string *stringREF = &brain;
+
+	std::cout << &brain << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	
+	std::cout << brain << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << *stringREF << std::endl;
+}
