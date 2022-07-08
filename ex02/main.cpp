9e3b3c3c25132805ac0446e17e9e10f7c2c379d3
swautelet <swautele@student.s19.be>
2022-07-08 12:43:37 +0200

    save ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,16 @@ int main()
 	std::string brain = "HI THIS IS BRAIN";
 
 	std::string *stringPTR = &brain;
-	std::string *stringREF = &brain;
+	std::string& stringREF = brain;
 
 	std::cout << &brain << std::endl;
 	std::cout << stringPTR << std::endl;
-	std::cout << stringREF << std::endl;
+	std::cout << &stringREF << std::endl;
 	
 	std::cout << brain << std::endl;
 	std::cout << *stringPTR << std::endl;
-	std::cout << *stringREF << std::endl;
+	std::cout << stringREF << std::endl;
+
+	// std::transform(stringREF.begin(), stringREF.end(), stringREF.begin(), ::tolower);
+	// std::cout << brain << std::endl;
 }
